4dc6fce08822a9413c182e3eb7c836bd26a8e508
Siddharth Roy <siddharthroy36912@gmail.com>
2020-11-27 13:44:15 +0530

    First init

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const http = require('http')
+const socketio = require('socket.io')
+const formatMessage = require('./utils/message')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/user')
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+// Set static folder
+app.use(express.static('public'))
+
+const botName = 'Mr Bot'
+
+// Run when client connect
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room)
+
+        socket.join(user.room)
+
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to LiteCord'))
+
+        // Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${username} has joined the chat`));
+        
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    })
+
+    
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id)
+
+        io.to(user.room).emit('message', formatMessage(user.username,  msg))
+    })
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+            
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
